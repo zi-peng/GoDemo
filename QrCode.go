e8d437169b107e4fb6a61c1ae1241dca943f6411
Administrator <Administrator@USER-20200401JS>
2020-07-21 14:09:06 +0800

    11

--- QrCode.go
+++ QrCode.go
@@ -1,3 +1,11 @@
 package main
 
-//
+import qrcode "github.com/skip2/go-qrcode"
+import "fmt"
+
+func main() {
+	err := qrcode.WriteFile("http://shop.zk-mall.net?userId=556", qrcode.Medium, 256, "qr.png")
+	if err != nil {
+		fmt.Println("write error")
+	}
+}
